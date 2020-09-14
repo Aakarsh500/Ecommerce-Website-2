e7e02e31f228329c8fd30e65d92dae4e76943db0
Aakarsh500 <64209522+Aakarsh500@users.noreply.github.com>
2020-09-14 22:53:53 +0530

    Added react-router-dom into our webpage but haven't really done anything

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./styles.css";
 import { HomePage } from "./Components/Pages/homepage/homepage.component";
 export default function App() {
+  const Hats = () => {
+    return <h1>Hats</h1>;
+  };
   return (
     <div>
-      <HomePage />
+      <Route exact={true} path="/" component={HomePage} />
+      {/* <HomePage /> */}
+      <Route exact path="/hats" component={Hats} />
     </div>
   );
 }
